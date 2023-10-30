1957c7939b3c351ad78dfeaa74719c08d3d7fa13
Warren Gonzalez <warren0419@outlook.com>
2023-10-29 21:30:37 -0600

    Creación del proyecto y repositorio

---
+++ src/main/java/com/outletcenter/OutletcenterApplication.java
@@ -0,0 +1,13 @@
+package com.outletcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutletcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutletcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outletcenter/OutletcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outletcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutletcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
