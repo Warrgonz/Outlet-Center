aebb56d44ff3088157aeea4f29a3b33216ec360e
Warren Gonzalez <warren0419@outlook.com>
2023-11-12 22:39:57 -0600

    Vista Admin
    Primeras visualizaciones.

--- src/main/java/com/outletcenter/OutletcenterApplication.java
+++ src/main/java/com/outletcenter/OutletcenterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OutletcenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OutletcenterApplication.class, args);
+		SpringApplication.run(OutletcenterApplication.class, args);            
 	}
 
 }
